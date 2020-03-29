e9a375bfb308f87f6c05096b035640b5d4b1d768
unknown <suzieboo@gmail.com>
2020-03-29 15:41:58 +1300

    USD

--- ec2-budget.tf
+++ ec2-budget.tf
@@ -2,7 +2,7 @@ resource "aws_budgets_budget" "ec2" {
   name              = "budget-ec2-daily"
   budget_type       = "COST"
   limit_amount      = "10"
-  limit_unit        = "AUD"
+  limit_unit        = "USD"
   time_period_end   = "2087-06-15_00:00"
   time_period_start = "2020-03-28_00:00"
   time_unit         = "MONTHLY"
